72ba0de0d269ab9060f3ad35343ee21face5fb86
wei <39419358@qq.com>
2023-08-22 15:24:58 +0800

    add rest client

---
+++ rest/client.go
@@ -0,0 +1,84 @@
+package rest
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Client struct {
+	BaseUrl       string
+	Authorization string
+	Debug         bool
+}
+
+func (c *Client) Get(path string, v any) *Error {
+	return c.doRequest(path, http.MethodGet, nil, v)
+}
+
+func (c *Client) Post(path string, req any, v any) *Error {
+	return c.doRequest(path, http.MethodPost, req, v)
+}
+
+func (c *Client) Patch(path string, req any, v any) *Error {
+	return c.doRequest(path, http.MethodPost, req, v)
+}
+
+func (c *Client) Delete(path string, v any) *Error {
+	return c.doRequest(path, http.MethodDelete, nil, v)
+}
+
+func (c *Client) doRequest(path string, method string, req any, v any) *Error {
+	var reqBody = new(bytes.Buffer)
+	json.NewEncoder(reqBody).Encode(req)
+
+	var hc http.Client
+	request, err := http.NewRequest(method, c.BaseUrl+path, reqBody)
+	if err != nil {
+		return &Error{StatusCode: 0, ErrCode: ErrCodeNetwork, Message: err.Error()}
+	}
+	request.Header.Set("Content-Type", "application/json")
+	if c.Authorization != "" {
+		request.Header.Set("Authorization", c.Authorization)
+	}
+
+	res, err := hc.Do(request)
+	if err != nil {
+		return &Error{StatusCode: 0, ErrCode: ErrCodeNetwork, Message: err.Error()}
+	}
+
+	var buf = new(bytes.Buffer)
+	io.Copy(buf, res.Body)
+	defer func() {
+		fmt.Printf("%s: %s, req: %s, resp: %s", method, path, reqBody.String(), buf.String())
+	}()
+
+	if res.StatusCode > http.StatusCreated {
+		return newClientErrorFromResponse(res.StatusCode, buf.Bytes())
+	}
+	err = json.Unmarshal(buf.Bytes(), v)
+	if err != nil {
+		return &Error{StatusCode: 0, ErrCode: ErrCodeBadResponseBody, Message: err.Error()}
+	}
+	return nil
+}
+
+func newClientErrorFromResponse(statusCode int, body []byte) *Error {
+	var ce = &Error{
+		StatusCode: statusCode,
+		ErrCode:    ErrCodeOk,
+		Message:    "",
+	}
+	if statusCode > http.StatusCreated {
+		err2 := json.Unmarshal(body, ce)
+		if err2 != nil {
+			ce.Message = err2.Error()
+			ce.ErrCode = ErrCodeBadResponseBody
+			return ce
+		}
+	}
+
+	return ce
+}
---
+++ rest/errors.go
@@ -0,0 +1,62 @@
+package rest
+
+const (
+	ErrUnauthorized        = "ErrUnauthorized"
+	ErrCodeNetwork         = "ErrNetwork"
+	ErrCodeInternalError   = "ErrInternalError"
+	ErrCodeBadResponseBody = "ErrBadResponseBody"
+	ErrCodeOk              = "Ok"
+)
+
+type Error struct {
+	StatusCode int    `json:"-"`
+	ErrCode    string `json:"err_code,omitempty"`
+	Message    string `json:"message,omitempty"`
+}
+
+func (e *Error) Status(status int) *Error {
+	e.StatusCode = status
+	return e
+}
+
+func (e *Error) Err(err string) *Error {
+	e.ErrCode = err
+	return e
+}
+
+func (e *Error) Error() string {
+	return e.String()
+}
+
+func (e *Error) String() string {
+	if e == nil {
+		return "<nil>"
+	}
+	//return fmt.Sprintf("status=%d, err_code=%s, err_msg=%s", e.StatusCode, e.ErrCode, e.Message)
+	return e.Message
+}
+
+func IsRestError(err error) (*Error, bool) {
+	if err == nil {
+		return nil, false
+	}
+
+	ce, ok := err.(*Error)
+	return ce, ok
+}
+
+func NewError(statusCode int, errMsg string) *Error {
+	return &Error{
+		StatusCode: statusCode,
+		ErrCode:    "",
+		Message:    errMsg,
+	}
+}
+
+func NewErrorWithCode(statusCode int, errCode string, errMsg string) *Error {
+	return &Error{
+		StatusCode: statusCode,
+		ErrCode:    errCode,
+		Message:    errMsg,
+	}
+}
